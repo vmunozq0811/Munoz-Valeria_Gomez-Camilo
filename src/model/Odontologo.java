ad7f8ff79c9cec3571bbf408d9b9b855cb1c3c3b
Camilo Gomez <camilogomez1014@hotmail.com>
2024-05-16 19:39:03 -0500

    Clase odontologo completada
    clase odontologo completa

--- src/model/Odontologo.java
+++ src/model/Odontologo.java
@@ -6,4 +6,39 @@ public class Odontologo {
     private Integer matricula;
     private String nombre;
     private String apellido;
+
+    public Odontologo(String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Odontologo(Integer id, String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
